16530d36a1c9a2c3e601f888985aebdaf904b780
chyngyzs1 <0777755169ch@gmail.com>
2019-06-12 11:46:45 +0600

    task 8 is done

---
+++ task8.py
@@ -0,0 +1,44 @@
+
+#   WRITE YOUR CODE HERE...
+def choice_to_number(choice):
+  if choice == 'Usain':
+    return 1
+  elif choice == 'Me':
+    return 2
+  else:
+    return 3
+
+# If choice is Usain you should get back 1.
+def number_to_choice(number):
+  if number == 1:
+    return 'Usain'
+  elif number == 2:
+    return 'Me'
+  else:
+    return 'Aybek'
+
+# if number is 1 then return usain bolt.
+#DO NOT remove lines below here,
+#this is designed to test your code.
+def test_choice_to_number():
+  assert choice_to_number('Usain') == 1
+  assert choice_to_number('Me') == 2
+  assert choice_to_number('Aybek') == 3
+
+def test_number_to_choice():
+  assert number_to_choice(1) == 'Usain'
+  assert number_to_choice(2) == 'Me'
+  assert number_to_choice(3) == 'Aybek'
+
+def test_all():
+  try:
+    test_choice_to_number()
+    test_number_to_choice()
+
+  except AssertionError:
+    print("wrong")
+
+  else:
+    print ("success")
+#test your code by un-commenting the line(s) below
+test_all()
